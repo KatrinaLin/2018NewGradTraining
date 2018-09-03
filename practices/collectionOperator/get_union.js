9e1052efcbc6a59be10c0b2c1a2f41ba75545113
katrinalin <zhaoyanlin2016@gmail.com>
2018-09-03 17:25:49 +0800

    complete the practices for collectionOperator

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -2,6 +2,8 @@
 
 function get_union(collection_a, collection_b) {
   //在这里写入代码
+  console.log()
+  return collection_a.concat(collection_b.filter(num => !collection_a.includes(num)));
 }
 
 module.exports = get_union;
