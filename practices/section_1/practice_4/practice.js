6de05bce6f15f1e00f1e958886b1ba8371f0b4c9
katrinalin <zhaoyanlin2016@gmail.com>
2018-09-04 01:26:23 +0800

    complete section_1

--- practices/section_1/practice_4/practice.js
+++ practices/section_1/practice_4/practice.js
@@ -1,5 +1,7 @@
 function collect_same_elements(collection_a, object_b) {
   //在这里写入代码
+  let a_key = collection_a.map(element => element.key);
+  return a_key.filter(x => object_b.value.includes(x));
 }
 
 module.exports = collect_same_elements;
