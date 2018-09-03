51e824cfc9dd8e7805f5d88edc662e78ee9c06aa
katrinalin <zhaoyanlin2016@gmail.com>
2018-09-04 01:26:15 +0800

    complete section_2

--- practices/section_2/practice_1/practice.js
+++ practices/section_2/practice_1/practice.js
@@ -1,5 +1,13 @@
 function count_same_elements(collection) {
   //在这里写入代码
+  return Object.values(collection.reduce((acc, ele) => {
+    if (ele in acc) {
+      acc[ele].count++;
+    } else {
+      acc[ele] = {key: ele, count: 1};
+    }
+    return acc;
+  }, {}))
 }
 
 module.exports = count_same_elements;
--- practices/section_2/practice_2/practice.js
+++ practices/section_2/practice_2/practice.js
@@ -1,5 +1,22 @@
 function count_same_elements(collection) {
   //在这里写入代码
+  return Object.values(collection.reduce((acc, ele) => {
+    let ele_count = 1;
+    let ele_key = ele;
+
+    if (ele.indexOf('-') >= 0) {
+      let arr = ele.split('-');
+      ele_key = arr[0];
+      ele_count = parseInt(arr[1]);
+    }
+
+    if (ele in acc) {
+      acc[ele_key].count += ele_count;
+    } else {
+      acc[ele_key] = {key: ele_key, count: ele_count};
+    }
+    return acc;
+  }, {}))
 }
 
 module.exports = count_same_elements;
