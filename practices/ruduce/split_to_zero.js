2dd48e06d379d2796cc17d35a4d38ff44d0fe493
katrinalin <zhaoyanlin2016@gmail.com>
2018-09-04 00:13:08 +0800

    complete reduce practices

--- practices/ruduce/split_to_zero.js
+++ practices/ruduce/split_to_zero.js
@@ -2,6 +2,13 @@
 
 function spilt_to_zero(number, interval) {
   //在这里写入代码
+  let result = [];
+  while (Math.round(number * 10) / 10 > 0) {
+    result.push(Math.round(number * 10) / 10);
+    number -= interval;
+  }
+  result.push(Math.round(number * 10) / 10);
+  return result;
 }
 
 module.exports = spilt_to_zero;
