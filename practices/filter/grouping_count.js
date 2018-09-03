484c75fde51d576e9ac1603a8d859633adbc2966
katrinalin <zhaoyanlin2016@gmail.com>
2018-09-04 00:11:24 +0800

    complete filter practices

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -3,6 +3,15 @@
 function grouping_count(collection) {
 
   //在这里写入代码
+  return collection.reduce(function (acc, num) {
+    if (num in acc) {
+      acc[num]++;
+    } else {
+      acc[num] = 1;
+    }
+    return acc;
+  }, {});
+
 }
 
 module.exports = grouping_count;
--- practices/filter/two_collections_practice_3.js
+++ practices/filter/two_collections_practice_3.js
@@ -3,6 +3,7 @@
 function choose_divisible_integer(collection_a, collection_b) {
 
   //在这里写入代码
+  return collection_a.filter(num_a => collection_b.find(num_b => num_a % num_b === 0));
 }
 
 module.exports = choose_divisible_integer;
