67b91b82b28e761340052749b62a3928749392be
katrinalin <zhaoyanlin2016@gmail.com>
2018-09-04 02:24:16 +0800

    complete exercises

--- practices/section_2/practice_3/practice.js
+++ practices/section_2/practice_3/practice.js
@@ -4,16 +4,20 @@ function count_same_elements(collection) {
     let ele_count = 1;
     let ele_key = ele;
 
-    if (ele.indexOf('-') >= 0 || ele.indexOf(':') >= 0 || ele.indexOf('[') >= 0) {
-      let arr = ele.split(/-/);
+    if (ele.indexOf('-') >= 0 || ele.indexOf(':') >= 0) {
+      let arr = ele.split(/[:-]/);
       ele_key = arr[0];
       ele_count = parseInt(arr[1]);
+    } else if (ele.indexOf('[') >= 0) {
+      ele_key = ele.substr(0, ele.indexOf('['));
+      ele_count = parseInt(ele.substr(ele.indexOf('[') + 1, ele.indexOf(']')));
+      console.log("key: " + ele_key + " count: " + ele_count)
     }
 
-    if (ele in acc) {
-      acc[ele_key].count += ele_count;
+    if (ele_key in acc) {
+      acc[ele_key].summary += ele_count;
     } else {
-      acc[ele_key] = {key: ele_key, count: ele_count};
+      acc[ele_key] = {name: ele_key, summary: ele_count};
     }
     return acc;
   }, {}))
