03c9bff4d94877fb5f4e2f23ba8e538416a98927
katrinalin <zhaoyanlin2016@gmail.com>
2018-09-04 00:11:58 +0800

    complete flaten practices

--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -3,6 +3,9 @@
 function double_to_one(collection) {
 
   //在这里写入代码
+  var flatResult = collection.reduce((acc, cur) => acc.concat(cur),[]);
+
+  return flatResult.filter((x, index, arr) => arr.indexOf(x) == index);
 }
 
 module.exports = double_to_one;
