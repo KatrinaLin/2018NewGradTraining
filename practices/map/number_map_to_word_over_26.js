1813c1ae36de1cc53983271a67e9df4383a67ddd
katrinalin <zhaoyanlin2016@gmail.com>
2018-09-04 00:12:26 +0800

    complete map practices

--- practices/map/number_map_to_word_over_26.js
+++ practices/map/number_map_to_word_over_26.js
@@ -1,6 +1,14 @@
 'use strict';
 var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+  return collection.map(function (x) {
+    let cur = "";
+    x--;
+    while (x >= 0) {
+      cur = String.fromCharCode(97 + x % 26) + cur;
+      x = Math.floor(x / 26) - 1;
+    }
+    return cur;
+  });
 };
 
 module.exports = number_map_to_word_over_26;
